421f043a52c54935c1d9736c0288dec05bd98f9c
lian1925 <lian1925@qq.com>
2019-05-06 17:53:36 +0800

    调试 webpack

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -37,18 +37,18 @@ module.exports = {
         include: path.resolve(__dirname, "../src"),
         use: [
           { loader: "cache-loader" },
-          {
-            loader: "thread-loader",
-            options: {
-              // there should be 1 cpu for the fork-ts-checker-webpack-plugin
-              workers: require("os").cpus().length,
-              poolTimeout: Infinity // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
-            }
-          },
+          // {
+          //   loader: "thread-loader",
+          //   options: {
+          //     // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+          //     workers: require("os").cpus().length - 1,
+          //     poolTimeout: Infinity // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
+          //   }
+          // },
           {
             loader: "ts-loader",
             options: {
-              transpileOnly: true,
+              transpileOnly: false,
               experimentalWatchApi: true,
               happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
             }
@@ -74,6 +74,10 @@ module.exports = {
             loader: "less-loader"
           }
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: "file-loader"
       }
     ]
   },
